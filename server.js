197299df27dbaf0137e66f3900cf8384e91cf14d
jamesgayflor <shekep1000@gmail.com>
2024-09-05 06:30:02 +0000

    Updates

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const server = express();
 const port = 1000;
 
 server.listen(port, () => {
-    console.log(`LodgeLiberia is running on port ${port}`)
+    console.log(`LodgeLiberia is running on port ${port}`);
 })
